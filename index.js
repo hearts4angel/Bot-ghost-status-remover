7a2ebd72ab5d7fcdce7b3dd7c827b073b1e95806
hearts4angel <146677251+hearts4angel@users.noreply.github.com>
2025-01-15 12:50:51 -0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["discord.gg/luvr", "Playing VALORANT"];
+const statusMessages = ["Playing VALORANT"];
 const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
